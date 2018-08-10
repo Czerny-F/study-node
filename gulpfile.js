268f771ad7a6e27819ef7a2b0f76401c482053d9
Czerny-F <dummy00.cz@gmail.com>
2018-08-10 22:34:54 +0900

    gulp: alt task

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,9 @@ var gulp = require('gulp');
 gulp.task('default', function() {
 	console.log('Hello World');
 });
+
+gulp.task('alt', (callback) => {
+  console.log(callback);
+  console.log(callback());
+  console.log('Hello Alt-World');
+});
